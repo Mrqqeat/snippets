02dcfd589441a729ebf83ec5d67b68068157eb23
Rachel Wall <solistra@gmx.com>
2014-07-04 17:12:54 -0400

    Generalize regular expression.

--- readme/readme.rb
+++ readme/readme.rb
@@ -2,7 +2,7 @@
 
 ARGV.each do |file|
   File.open(File.join(File.dirname(file), '..', 'README.md'), 'w') do |readme|
-    File.read(file) =~ /\A#-{2}(.+)#\+{2}/m
+    File.read(file) =~ /#-{2}(.+)#\+{2}/m
     readme << $1.gsub!(/^# /, '')
     puts "Generated README.md file (#{File.expand_path(readme)})"
   end
